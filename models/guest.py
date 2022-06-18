33527b06e9b36b74643bf5412be852b170db28ae
root <gveitia13@gmail.com>
2022-06-18 05:13:05 -0500

    create persona model

--- models/guest.py
+++ models/guest.py
@@ -1,24 +1,24 @@
 import string
 from odoo import models, fields
 
+
 class Guest(models.Model):
     _name = "hotel.guest"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     # _recname = 'identified'
 
-    
-    name = fields.Char("Nombre")
+    name = fields.Char("Nombre asd")
     lastname = fields.Char("Apellidos")
     origin = fields.Char("Lugar de Procedencia")
     motive = fields.Text("Motivo")
     identified = fields.Integer("Numero de Identificacion", required=False)
     init_date = fields.Date(
-       'Fecha de Entrada',
-       required=True,
+        'Fecha de Entrada',
+        required=True,
     )
     end_date = fields.Date(
-       'Fecha de Salida',
-       required=True,
+        'Fecha de Salida',
+        required=True,
     )
     apartment_id = fields.Many2one('hotel.apartment', string="Apartamento")
     request_ids = fields.Many2many('hotel.request', string="Solicitudes")
--- models/persona.py
+++ models/persona.py
@@ -5,6 +5,7 @@ class Persona(models.Model):
     _name = "hotel.persona"
     _description = 'Persona del SGU'
 
+    # Información Nominal
     tipo_identidad = fields.Selection([
         ('cubana', 'Cubana'),
         ('extranjera', 'Extranjera'), ],
@@ -12,7 +13,7 @@ class Persona(models.Model):
         default='cubana',
         required=True
     )
-    carnet = fields.Char('Número de carnet', required=True)  # arreglar esto
+    carnet = fields.Float('Número de carnet', required=True, size=11, digits=(11, 0))  # arreglar esto
     serie_carnet = fields.Integer('Número de serie CI', required=True)
     provincia = fields.Selection([
         ('havana', 'Havana'),
@@ -50,4 +51,145 @@ class Persona(models.Model):
         default='cubana',
         required=True
     )
-    foto = fields.Binary(string='Foto')
+    foto = fields.Image(string='Foto')
+    # Dirección Particular
+    dir_permanente = fields.Char('Residencia permanente')
+    dir_numero = fields.Integer('No')
+    dir_apartamento = fields.Char('Apartamento')
+    dir_provincia = fields.Selection([
+        ('havana', 'Havana'),
+        ('pinar_del_rio', 'Pinar del Río'),
+        ('cienfuegos', 'Cienfuegos'),
+        ('granma', 'Granma'),
+    ],
+        string='Provincia registro civil',
+        required=True)
+
+    dir_municipio = fields.Selection([
+        ('marianao', 'Marianao'),
+        ('centro_habana', 'Centro Habana'),
+        ('playa', 'playa'),
+    ],
+        string='Registro civil',
+        required=True)
+    dir_localidad = fields.Char('Localidad')
+    dir_entre = fields.Char('Entre')
+    dir_circunscripcion = fields.Char('Circunscripción')
+    dir_km = fields.Char('Km')
+    dir_cpa = fields.Char('CPA')
+    dir_carretera = fields.Char('Carretera')
+    dir_edificio = fields.Char('Edificio')
+    dir_pais = fields.Selection([
+        ('cuba', 'Cuba'),
+        ('eeuu', 'EEUU'),
+        ('angola', 'Angola'),
+        ('vietnam', 'VietNam'),
+    ],
+        string='Provincia registro civil',
+        required=True)
+    dir_finca = fields.Char('Finca')
+    # Rasgos fenotípicos
+    rasgo_sexo = fields.Selection([
+        ('f', 'Femenino'),
+        ('m', 'Masculino'),
+        ('otro', 'Otro'),
+    ],
+        string='Sexo',
+        required=True)
+    rasgo_raza = fields.Selection([
+        ('b', 'Blanca'),
+        ('m', 'Mestizo'),
+        ('n', 'Negro'),
+    ],
+        string='Raza', )
+    rasgo_ojos = fields.Selection([
+        ('a', 'Azul'),
+        ('v', 'Verde'),
+        ('n', 'Negro'),
+        ('c', 'Carmelita'),
+    ],
+        string='Color de ojos', )
+    rasgo_pelo = fields.Selection([
+        ('r', 'Rubio'),
+        ('ca', 'Castaño'),
+        ('n', 'Negro'),
+        ('c', 'Carmelita'),
+    ],
+        string='Color de pelo', )
+    rasgo_peso = fields.Char('Peso (Kg)')
+    rasgo_estatura = fields.Char('Estatura (cm)')
+    rasgo_discapacidad = fields.Selection([
+        ('c', 'Ciego'),
+        ('s', 'Sordo'),
+        ('m', 'Mudo'),
+        ('i', 'Inválido'),
+    ],
+        string='Tipo de discapacidad', )
+    # Datos personales
+    datos_estado_civil = fields.Selection([
+        ('c', 'Casado'),
+        ('s', 'Soltero'),
+    ],
+        string='Estado civil', )
+    datos_hijos = fields.Integer('Cantidad de hijos')
+    datos_huerfano = fields.Selection([
+        ('p', 'Padre'),
+        ('m', 'Madre'),
+        ('a', 'Ambos'),
+    ],
+        string='Huérfano', )
+    datos_id_expediente = fields.Char('Id de expediente')
+    datos_telefono_p = fields.Char('Teléfono fijo')
+    datos_correo = fields.Char('Correo')
+    datos_solapin = fields.Char('Solapín')
+    datos_telefono_c = fields.Char('Teléfono celular')
+    datos_fax = fields.Char('Fax')
+    datos_organizaciones_p = fields.Selection([
+        ('pcc', 'PCC'),
+        ('ujc', 'UJC'),
+        ('a', 'Ambos'),
+    ],
+        string='Organizaciones políticas', )
+    datos_organizaciones_m = fields.Selection([
+        ('fmc', 'FMC'),
+        ('cdr', 'CDR'),
+        ('a', 'Ambos'),
+    ],
+        string='Organizaciones de masa', )
+    # Datos de superación
+    super_nivel_escolar = fields.Selection([
+        ('bachiller', 'Bachiller'),
+        ('universitario', 'Universitario'),
+        ('master', 'Máster'),
+        ('doctor', 'Doctor'),
+    ],
+        string='Nivel escolar', )
+    super_categoria_docente = fields.Selection([
+        ('rga', 'RGA'),
+        ('queseyo', 'Que se yo'),
+        ('duro', 'Esta duro'),
+    ],
+        string='Categoría docente', )
+    super_profesion = fields.Selection([
+        ('ing', 'Ingeniero en Ciencias Informáticas'),
+        ('bio', 'Ingeniero en Bioinformática'),
+        ('algo', 'Algo mas'),
+    ],
+        string='Profesión', )
+    super_grado = fields.Selection([
+        ('9no', '9no grado'),
+        ('12mo', '12mo grado'),
+        ('universitario', 'universitario'),
+    ],
+        string='Grado académico')
+    super_categoria_investigativa = fields.Selection([
+        ('1', 'Lasdjsa'),
+        ('2', '1asrdsdfds'),
+        ('3', 'dsfdsfdsf'),
+    ],
+        string='Grado académico')
+    # Herencias
+    apartment_id = fields.Many2one('hotel.apartment', string="Apartamento")
+    request_ids = fields.Many2many('hotel.request', string="Solicitudes")
+    register_number = fields.Integer("Numero de Registro")
+    carpetera = fields.Char("Nombre de la Carpetera")
